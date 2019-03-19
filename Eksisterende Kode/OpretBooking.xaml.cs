8d78542083a9140fa4abc1956554a460acb76a6d
Anders Weiskvist <Weiskvist94@gmail.com>
2019-03-19 12:17:52 +0100

    OpretBooking side i gang

---
+++ Eksisterende Kode/OpretBooking.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eksisterende_Kode
+{
+    /// <summary>
+    /// Interaction logic for OpretBooking.xaml
+    /// </summary>
+    public partial class OpretBooking : Page
+    {
+        public OpretBooking()
+        {
+            InitializeComponent();
+        }
+    }
+}
